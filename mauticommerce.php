afb22d81bf44ed3f0e3d20eef50d064bca2d7f4c
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-10 22:19:04 +0900

    init

---
+++ mauticommerce.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Mauticommerce
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: mauticommerce
+ * Domain Path: /languages
+ * @package Mauticommerce
+ */
