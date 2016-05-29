9b08a3bd531c55d172e30b2d534bda39b485ca6b
hideokamoto <hide.okamoto@digitalcube.jp>
2016-05-29 22:36:10 +0900

    create activation tests

---
+++ tests/unit/activationTest.php
@@ -0,0 +1,11 @@
+<?php
+class activationTest extends WP_UnitTestCase {
+	public function setUp() {
+	}
+	public function tearDown() {
+	}
+	// tests
+	public function testIsActivatedWooCommercePlugin() {
+		$this->assertTrue( mautic_is_activate_woocommerce() );
+	}
+}
