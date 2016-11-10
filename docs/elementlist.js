19c67748ea022fc2aaac46b6151ddc5ff621eb6d
hideokamoto <hide.okamoto@digitalcube.jp>
2016-11-10 19:48:15 +0900

    create docs by apigen

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","__()"],["f","_x()"],["f","get_mautic_country_list()"],["f","get_mautic_region()"],["f","get_woocommerce_country_list()"],["f","mautic_is_activate_woocommerce()"],["c","Mauticommerce"],["c","Mauticommerce_Admin"],["c","Mauticommerce_Err"],["c","Mauticommerce_Order"]];
